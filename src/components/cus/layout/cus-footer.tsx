5cdb4aa8e051366a6583729062bf54b2994164fc
JIYUEDUSHANG <liuyongle_jyds@163.com>
2024-05-31 16:39:38 +0800

    style: footer-fix-h5

--- src/components/cus/layout/cus-footer.tsx
+++ src/components/cus/layout/cus-footer.tsx
@@ -36,31 +36,32 @@ export default function CusFooter({ dict }: Props) {
   ]
 
   return (
-    <footer className='pt-10'>
-      <div className='flex justify-between text-sm text-t2'>
-        <div className='w-[25rem]'>
+    <footer className='pt-5 md:pt-10'>
+      <div className='flex flex-wrap justify-between space-x-2 text-sm text-t2'>
+        <div className='mb-4 md:mb-5 md:w-[25rem]'>
           <Image
             src='/images/logo@2x.png'
             alt='AIToolGo logo'
             width={96}
             height={18}
-            className='w-24'
+            className='w-16 md:w-24'
             priority
           />
-          <div className='h-10'></div>
-          <div className='whitespace-pre-wrap'>{dict.footer.introduce}</div>
+          <div className='h-3 md:h-10'></div>
+          <div className='whitespace-pre-wrap text-sm md:text-base md:leading-6'>
+            {dict.footer.introduce}
+          </div>
         </div>
-        <div className='h-1 w-2'></div>
-        <div>
+        <div className='mb-4 md:mb-5'>
           <h2 className='text-base font-medium text-t1'>{dict.header.Tools}</h2>
-          <div className='h-5'></div>
+          <div className='h-2 md:h-5'></div>
           <ul>
             {toolsList.map((e, index) => (
               <li key={index}>
                 <Link
                   href={e.link!}
                   title={e.text}
-                  className='flex h-10 items-center hover:opacity-85'
+                  className='flex h-7 items-center hover:opacity-85 md:h-10'
                 >
                   {e.text}
                 </Link>
@@ -68,19 +69,18 @@ export default function CusFooter({ dict }: Props) {
             ))}
           </ul>
         </div>
-        <div className='h-1 w-2'></div>
-        <div>
+        <div className='mb-4 md:mb-5'>
           <h2 className='text-base font-medium text-t1'>
             {dict.header.Experience}
           </h2>
-          <div className='h-5'></div>
+          <div className='h-2 md:h-5'></div>
           <ul>
             {experienceList.map((e, index) => (
               <li key={index}>
                 <Link
                   href={e.link!}
                   title={e.text}
-                  className='flex h-10 items-center hover:opacity-85'
+                  className='flex h-7 items-center hover:opacity-85 md:h-10'
                 >
                   {e.text}
                 </Link>
@@ -88,17 +88,16 @@ export default function CusFooter({ dict }: Props) {
             ))}
           </ul>
         </div>
-        <div className='h-1 w-2'></div>
-        <div>
+        <div className='mb-4 md:mb-5'>
           <h2 className='text-base font-medium text-t1'>
             {dict.footer['Contact Us']}
           </h2>
           <ul>
             {mailList.map((e, index) => (
-              <li key={index} className='mt-5'>
+              <li key={index} className='mt-2'>
                 <Link
                   href={e.link}
-                  className='flex h-10 items-center hover:opacity-85'
+                  className='flex h-7 items-center hover:opacity-85 md:h-10'
                 >
                   {e.text}
                   <span className='underline'>{e.mail}</span>
@@ -106,20 +105,19 @@ export default function CusFooter({ dict }: Props) {
               </li>
             ))}
           </ul>
-          <div className='h-5'></div>
         </div>
       </div>
-      <div className='h-5'></div>
-      <div className='flex h-16 items-center justify-between'>
+      <div className='flex min-h-16 flex-wrap items-center justify-between'>
         <CusLanguage isGrey />
-        <div className='flex h-full items-center text-xs text-t3'>
+        <div className='flex h-full flex-wrap items-center text-xs leading-6 text-t3 md:leading-4'>
           <Link href=''>{dict.footer['Term of service']}</Link>
           <div className='h-1 w-5'></div>
           <Link href=''>{dict.footer['Privacy Agreement']}</Link>
-          <div className='h-1 w-10'></div>
+          <div className='h-1 w-5 md:w-10'></div>
           <span>© 2024 AIToolGo.com, Inc. All rights reserved</span>
         </div>
       </div>
+      <div className='h-5'></div>
     </footer>
   )
 }
